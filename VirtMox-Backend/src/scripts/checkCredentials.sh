17a004cf85cd1b7ff66453f41891a523544abc8b
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2024-03-28 14:55:24 +0100

    Fix hashing

--- VirtMox-Backend/src/scripts/checkCredentials.sh
+++ VirtMox-Backend/src/scripts/checkCredentials.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 # Generate a hashed password with the given salt and password
-function hash_password_sha512 {
-    password="$1"
-    salt="$2"
-    hashed_password=$(openssl passwd -6 -salt $salt $password)
-    echo $hashed_password
-}
+#function hash_password_sha512 {
+#    password="$1"
+#    salt="$2"
+#    hashed_password=$(openssl passwd -6 -salt $salt $password)
+#    echo $hashed_password
+#}
 
 # Generate a hashed password with the given salt and password using YesCrypt
-function hash_password_yescrypt {
-    password="$1"
-    salt="$2"
-    hashed_password=$(yescrypt "$password" "$salt")
-    echo $hashed_password
+#function hash_password_yescrypt {
+#    password="$1"
+#    salt="$2"
+#    hashed_password=$(yescrypt "$password" "$salt")
+#    echo $hashed_password
+#}
+
+function hash_password_crypt {
+    local password="$1"
+    local salt="$2"
+    local hashed_password=$(perl -e "print crypt('$password', '\$$salt\$')")
+    echo "$hashed_password"
 }
+
 
 # We need to read /etc/shadow later, so make sure we got the correct permissions
 if [ "$EUID" -ne 0 ]; then
@@ -49,15 +57,19 @@ hash=$(echo $line | cut -d ":" -f 2)
 
 alg=$(echo $hash | cut -c 1-3)
 
-if [[ "$alg" = '$6$' ]]; then
-  salt=$(echo $hash | cut -d "$" -f 3)
-  hashed_password=$(hash_password_sha512 "$password" "$salt") 
-elif [[ "$alg" = '$y$' ]]; then
-  salt=$(echo $hash | cut -d "$" -f 3)
-  hashed_password=$(hash_password_yescrypt "$password" "$salt")
-elif [[ "$alg" = '$1$' ]]; then
-  echo "Found MD5"
-fi
+alg_and_salt=$(echo $hash | cut -d "$" -f 2-3)
+
+hashed_password=$(hash_password_crypt "$password" "$alg_and_salt")
+
+#if [[ "$alg" = '$6$' ]]; then
+#  salt=$(echo $hash | cut -d "$" -f 3)
+#  hashed_password=$(hash_password_sha512 "$password" "$salt") 
+#elif [[ "$alg" = '$y$' ]]; then
+#  salt=$(echo $hash | cut -d "$" -f 3)
+#  hashed_password=$(hash_password_yescrypt "$password" "$salt")
+#elif [[ "$alg" = '$1$' ]]; then
+#  echo "Found MD5"
+#fi
 
 # Compare the resulting hash with the stored hash
 if [[ "$hashed_password" = "$hash" ]]; then
